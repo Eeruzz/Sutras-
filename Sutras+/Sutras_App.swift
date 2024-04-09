836e565d7f68f9aed6ef52e7c266d9cef255f62e
Eeruzz <irisvanwuijckhuijse@gmail.com>
2024-04-09 10:56:17 -0500

    Initial Commit

---
+++ Sutras+/Sutras_App.swift
@@ -0,0 +1,17 @@
+//
+//  Sutras_App.swift
+//  Sutras+
+//
+//  Created by 64015148 on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Sutras_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
